9c6a0569a9947a3850af905e6eba1038d516caba
Daidv <dvdai63@gmail.com>
2023-02-17 21:41:13 +0700

    update config react router

--- app/javascript/packs/react-app.jsx
+++ app/javascript/packs/react-app.jsx
@@ -1,22 +1,18 @@
-// Run this by adding <%= javascript_pack_tag 'react-app' %> to the head of your layout file,
+// // Run this by adding <%= javascript_pack_tag 'react-app' %> to the head of your layout file,
 
 
 import React from 'react'
+
 import { createRoot } from 'react-dom/client';
-import PropTypes from 'prop-types'
 import './react-app.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from '../src/components/shop/home'
-import Admin from '../src/components/admin'
-// import InstructorIndex from '../src/components/admin/instructor_index'
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/home' element={ <Home/> } />
-        <Route path='/admin' element={ <Home/> } />
-        {/* <Route path='instructors/new' element={ <Instructor/> } /> */}
       </Routes>
     </div>
   )
@@ -24,6 +20,8 @@ function App() {
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('react-app')).render(
-   <Admin />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   );
 })
--- app/javascript/src/components/shop/home/index.jsx
+++ app/javascript/src/components/shop/home/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import About from '../about';
-import ProductBlock from '../poduct_block';
+import ProductBlock from '../product_block';
 import './style.scss'
 const Home = () => {
   return (
