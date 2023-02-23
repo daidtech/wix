3f8dc4dd435d95de0b18a5c5117783a7ebb83124
Daidv <dvdai63@gmail.com>
2023-02-23 08:00:18 +0700

    add devise layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.create(name: 'admin', email: 'admin@gmail.com' ,password: '123123')
+admin.add_role :admin
